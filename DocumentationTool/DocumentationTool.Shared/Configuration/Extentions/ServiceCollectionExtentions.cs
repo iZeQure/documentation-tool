fe3b0f5a74e17f6de69157557e3265f596d8f14a
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2022-01-19 23:52:56 +0100

    prototyping/extention-methods
    - Added support for adding configuration settings.
    - A website helper, for going to a specific relative path on the current URL.

---
+++ DocumentationTool/DocumentationTool.Shared/Configuration/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DocumentationTool.Shared.Configuration.Extentions
+{
+    public static class ServiceCollectionExtentions
+    {
+        /// <summary>
+        /// Adds configuration settings into the <see cref="IServiceCollection"/> container.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns>The associated <see cref="IServiceCollection"/> collection, with the added settings.</returns>
+        /// <exception cref="ArgumentNullException" />
+        public static IServiceCollection AddConfigurationSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration is null)
+                throw new ArgumentNullException(nameof(configuration), "No valid configuration was given.");
+
+            IWebsiteConfigurationSettings webSettings = configuration.GetSection("WebSiteSettings").Get<WebsiteConfigurationSettings>();
+            IAuthenticationSettings authSettings = configuration.GetSection("AuthenticationSettings").Get<AuthenticationSettings>();
+
+            services.AddSingleton(webSettings);
+            services.AddSingleton(authSettings);
+
+            return services;
+        }
+    }
+}
---
+++ DocumentationTool/DocumentationTool.Shared/Helpers/WebsiteHelperExtentions.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DocumentationTool.Shared.Helpers
+{
+    public static class WebsiteHelperExtentions
+    {
+        private const int _helperTimeout = 5;
+
+        /// <summary>
+        /// Goes to the url, by a given relative path.
+        /// </summary>
+        /// <param name="webDriver"></param>
+        /// <param name="relativePath">Contains the path to associate at the end of the url.</param>
+        /// <returns>True when the location has changed, otherwise false.</returns>
+        public static bool GoToUrlByRelativePath(this IWebDriver webDriver, string relativePath)
+        {
+            Uri url = new(webDriver.Url + relativePath);
+
+            webDriver.Navigate().GoToUrl(url);
+
+            WebDriverWait wait = new(webDriver, TimeSpan.FromSeconds(_helperTimeout));
+
+            return wait.Until<bool>(driver => driver.Url.Contains(relativePath));
+        }
+    }
+}
