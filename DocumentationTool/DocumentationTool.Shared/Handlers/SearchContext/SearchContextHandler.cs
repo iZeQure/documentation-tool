5bc2860a56244f3db0ac00e04373a2125df37203
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2022-01-21 13:00:02 +0100

    prototyping/search-context
    - Removed faulty code.

--- DocumentationTool/DocumentationTool.Shared/Handlers/SearchContext/SearchContextHandler.cs
+++ DocumentationTool/DocumentationTool.Shared/Handlers/SearchContext/SearchContextHandler.cs
@@ -17,8 +17,6 @@ namespace DocumentationTool.Shared.Handlers.SearchContext
             _webDriver = webDriver;
         }
 
-        public void 
-
         private bool AddBookmarkForApprenticeshupByEducationSuffix(string suffix)
         {
             const string bookmarkedAttributeValue = "M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z";
